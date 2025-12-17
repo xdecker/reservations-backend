cbda134c31c09119f0196ace334c970965b3101e
xdecker <xavier_deckza@hotmail.com>
2025-12-17 12:10:03 -0500

    feat: validations of available time

--- app/Http/Requests/StoreReservationRequest.php
+++ app/Http/Requests/StoreReservationRequest.php
@@ -3,7 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-
+use App\Models\Space;
+use Carbon\Carbon;
 class StoreReservationRequest extends FormRequest
 {
     /**
@@ -27,5 +28,33 @@ class StoreReservationRequest extends FormRequest
             'start_time' => 'required|date|after:now',
             'end_time'   => 'required|date|after:start_time',
         ];
+    }
+
+    public function withValidator($validator)
+    {
+        $validator->after(function ($validator) {
+            $space = Space::find($this->space_id);
+            if ($space) {
+                $start = Carbon::parse($this->start_time);
+                $end   = Carbon::parse($this->end_time);
+
+                if ($start->format('H:i') < $space->available_from || $end->format('H:i') > $space->available_to) {
+                    $validator->errors()->add(
+                        'time', 
+                        'The reservation must be within the available hours of the space (' . $space->available_from . ' - ' . $space->available_to . ').'
+                    );
+                }
+
+                // Bloques de 30min para calendario limpio
+                if ($start->minute % 30 !== 0 || $end->minute % 30 !== 0) {
+                    $validator->errors()->add('time', 'Reservation must start and end in 30-minute blocks.');
+                }
+
+                // Que dure almenos 30min
+                if ($start->diffInMinutes($end) % 30 !== 0) {
+                    $validator->errors()->add('time', 'Reservation duration must be a block of 30 minutes.');
+                }
+            }
+        });
     }
 }
--- app/Http/Requests/UpdateReservationRequest.php
+++ app/Http/Requests/UpdateReservationRequest.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Http\Requests;
-
+use App\Models\Space;
+use Carbon\Carbon;
 use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateReservationRequest extends FormRequest
@@ -27,5 +28,33 @@ class UpdateReservationRequest extends FormRequest
             'start_time' => 'sometimes|date|after:now',
             'end_time'   => 'sometimes|date',
         ];
+    }
+
+    public function withValidator($validator)
+    {
+        $validator->after(function ($validator) {
+            $space = Space::find($this->space_id ?? $this->reservation->space_id);
+            if ($space) {
+                $start = Carbon::parse($this->start_time);
+                $end   = Carbon::parse($this->end_time);
+
+                if ($start->format('H:i') < $space->available_from || $end->format('H:i') > $space->available_to) {
+                    $validator->errors()->add(
+                        'time', 
+                        'The reservation must be within the available hours of the space (' . $space->available_from . ' - ' . $space->available_to . ').'
+                    );
+                }
+
+                // Bloques de 30min para calendario limpio
+                if ($start->minute % 30 !== 0 || $end->minute % 30 !== 0) {
+                    $validator->errors()->add('time', 'Reservation must start and end in 30-minute blocks.');
+                }
+
+                // Que dure almenos 30min
+                if ($start->diffInMinutes($end) % 30 !== 0) {
+                    $validator->errors()->add('time', 'Reservation duration must be a block of 30 minutes.');
+                }
+            }
+        });
     }
 }
